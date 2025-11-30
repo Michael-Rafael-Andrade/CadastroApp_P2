85e5ff41d02d070fd05d63638c5f86f8f195831d
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-30 11:55:30 -0300

    Implementando o CRUD: Cadastro com AsyncStorage
    Implementando o CRUD: Cadastro com AsyncStorage

--- telas/TelaCadastro.js
+++ telas/TelaCadastro.js
@@ -2,6 +2,7 @@
 // importação
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Importaremos esta biblioteca para formatação de data e cpf para o formatato escolhido como padrão.
 // Para isto vou instalar um pacote para auxiliar na formatação da mascara que usarei no meu projeto. 
@@ -9,8 +10,9 @@ import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-nat
 import MaskInput, { Masks } from 'react-native-mask-input';
 
 
+
 // exporta a função
-export function TelaCadastro(){
+export function TelaCadastro() {
     //Definir estado inicial 
     const [nome, setNome] = useState('');
     const [cpf, setCpf] = useState('');
@@ -18,22 +20,72 @@ export function TelaCadastro(){
     const [endereco, setEndereco] = useState('');
     const [telefone, setTelefone] = useState('');
 
-    // função para tratar o cadastro
-    const handleCadastrar = () => {
-        console.log('--- Dados do Novo Usuário ---');
-        console.log('Nome: ', nome);
-        console.log('CPF: ', cpf);
-        console.log('Data de nascimento: ', dataNascimento);
-        console.log('Endereco: ', endereco);
-        console.log('Telefone: ', telefone);
-        
 
-        // Limpar o formulário
-        setNome('');
-        setCpf('');
-        setDataNascimento('');
-        setEndereco('');
-        setTelefone('');
+    const getUsuarios = async () => {
+        try {
+            const jsonUsuarios = await AsyncStorage.getItem('USUARIOS_CADASTRADOS');
+            // Se houver dados, retorna a lista parseada, senão, retorna um array vazio
+            return jsonUsuarios != null ? JSON.parse(jsonUsuarios) : [];
+        } catch (erro) {
+            console.error("Erro ao buscar usuários: ", erro);
+            return [];
+        }
+    };
+
+    // função para tratar o cadastro
+    const handleCadastrar = async () => {
+        // validação simples
+        if (!nome || !cpf || !dataNascimento || !endereco || !telefone) {
+            alert("Preencha todos os campos antes de cadastrar!");
+            return;
+        }
+
+        // Gerar um ID único - simples (Timestamp)
+        const novoId = Date.now().toString();
+
+        // Criar o objeto do novo usuário
+        const novoUsuario = {
+            id: novoId,
+            nome: nome,
+            cpf: cpf,
+            dataNascimento: dataNascimento,
+            endereco: endereco,
+            telefone: telefone,
+        };
+
+        try {
+            // Bucar a lista atual de usuários
+            const listaAtual = await getUsuarios();
+
+            // Adicionar o novo usuário na lista ou à lista
+            listaAtual.push(novoUsuario);
+
+            // Salvar a lista atualizada no AsyncStorage (convertendo para string)
+            await AsyncStorage.setItem('USUARIOS_CADASTRADOS', JSON.stringify(listaAtual));
+
+            alert('Usuário cadastrado com sucesso!');
+
+            // Limpar o formulário
+            // Limpar o formulário
+            setNome('');
+            setCpf('');
+            setDataNascimento('');
+            setEndereco('');
+            setTelefone('');
+
+        } catch (erro){
+            console.error('Erro ao salvar o usuário: ', erro);
+            alert('Falha ao salvar o usuário. Tente novamente. ');
+        }
+
+        // Não irei mais precisar deste trecho de código.
+        // console.log('--- Dados do Novo Usuário ---');
+        // console.log('Nome: ', nome);
+        // console.log('CPF: ', cpf);
+        // console.log('Data de nascimento: ', dataNascimento);
+        // console.log('Endereco: ', endereco);
+        // console.log('Telefone: ', telefone);
+
     };
 
     return (
@@ -63,7 +115,7 @@ export function TelaCadastro(){
                 style={estilos.entrada}
                 value={dataNascimento}
                 onChangeText={(masked, unmasked) => setDataNascimento(masked)}
-            mask={MaskInput.DATE_DDMMYYYY}
+                mask={MaskInput.DATE_DDMMYYYY}
             />
 
             <Text style={estilos.rotulo}>Endereço:</Text>
@@ -127,7 +179,7 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         backgroundColor: '#f9f9f9',
     },
-    botaoContainer:{
+    botaoContainer: {
         marginTop: 20,
         marginBottom: 40,
     },
